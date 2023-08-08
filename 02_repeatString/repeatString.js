cbcd07574ebfa637711c9b1104704a9eace4f25a
cyberace313 <zafer.arifi290@gmail.com>
2023-08-08 08:54:12 +0430

    Complete all the seven tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let result = str;
+  for (let i = 1; i < num; i++) {
+    result += str;
+  }
+  if (num === 0 || str === "") {
+    result = "";
+  } else if (num < 0) {
+    result = "ERROR";
+  }
+  return result;
 };
+
+function repeatRandomTimes(str) {
+  let result = str;
+  randomNum = Math.floor(Math.random() * 1000);
+  for (let i = 1; i < randomNum; i++) {
+    result += str;
+  }
+  if (str === "") {
+    result = "";
+  }
+  return result;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
