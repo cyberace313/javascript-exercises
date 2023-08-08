d1446e50ff7c1e91ab6c3abf466da1a2627baf88
cyberace313 <zafer.arifi290@gmail.com>
2023-08-08 11:48:19 +0430

    Pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  let sumArr = [];
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  } else if (typeof start !== "number" || typeof end !== "number") {
+    return "ERROR";
+  } else if (start > end) {
+    for (let i = end; i <= start; i++) {
+      sumArr.push(i);
+    }
+  } else {
+    for (let i = start; i <= end; i++) {
+      sumArr.push(i);
+    }
+  }
+  let sum = sumArr.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  return sum;
 };
 
 // Do not edit below this line
