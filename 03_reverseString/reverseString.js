2a98a256df2d040e58caf4290995f8be892ed507
cyberace313 <zafer.arifi290@gmail.com>
2023-08-08 09:14:19 +0430

    Complete all the four tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  let characters = str.split("");
+  let reversed = characters.reverse();
+  let joinedString = reversed.join("");
 
+  return joinedString;
 };
 
 // Do not edit below this line
